7c3f30ee0ce2d526189cdcbaf4f7b45260c60833
matej-hron <matej.hron@gim>
2017-06-11 07:55:45 +0200

    linear_regression

--- reshape.py
+++ reshape.py
@@ -1,6 +1,7 @@
 from keras.datasets import mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-print(train_images)
+
 train_images = train_images.reshape((60000, 28 * 28)) 
 train_images = train_images.astype('float32') / 255
+
 print(train_images)
--- reshape2.py
+++ reshape2.py
@@ -1,13 +1,13 @@
-import numpy as np 
-x = np.array(12) 
+import numpy as np
+x = np.array(12)
 print(x.ndim)
 
-x = np.array([12, 3, 6, 14]) 
+x = np.array([12, 3, 6, 14])
 print(x.ndim)
 
-x = np.array([[5, 78, 2, 34, 0],                  
-              [6, 79, 3, 35, 1],                  
-              [7, 80, 4, 36, 2]]) 
+x = np.array([[5, 78, 2, 34, 0],
+              [6, 79, 3, 35, 1],
+              [7, 80, 4, 36, 2]])
 print(x.ndim)
 print(x.shape)
 
@@ -18,5 +18,5 @@ print(x)
 
 x = x.reshape(5 * 3)
 print(x)
-x = x.astype('float32') *2
+x = x.astype('float32') * 2
 print(x)
